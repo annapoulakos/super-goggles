2d2f690116a4952c0437773126fbdf487b67c665
James Poulakos <admin@jimpoulakos.com>
2016-11-23 06:39:35 -0600

    feat: force miners to choose single source

--- role.miner.js
+++ role.miner.js
@@ -1,19 +1,43 @@
+var Utility = require('utility');
+
 var Miner = {
     sourceId: null,
     execute: function (creep) {
         var sources = creep.room.find(FIND_SOURCES);
 
-        for (var id in sources) {
-            var source = sources[id];
+        if (!creep.memory.harvestSourceId) {
+            var miners = Utility.getCreepsByRole('miner');
+            var takenIds = miners.map(m => m.memory.harvestSourceId);
 
+            var sources = creep.room.find(FIND_SOURCES, {filter: s => {
+                return takenIds.map(id => s.id != id).reduce((a,b) => a&b);
+            }});
+
+            if (sources.length) {
+                creep.memory.harvestSourceId = sources[0].id;
+            }
+        }
+
+        var source = Game.getObjectById(creep.memory.harvestSourceId);
+        if (source) {
             var harvest = creep.harvest(source);
 
             switch (harvest) {
-                case ERR_NOT_IN_RANGE: creep.moveTo(source); creep.say('Moving'); return;
-                case OK: return;
-                default: break;
+                case ERR_NOT_IN_RANGE: creep.moveTo(source); creep.say('Moving'); break;
             }
         }
+
+        // for (var id in sources) {
+        //     var source = sources[id];
+
+        //     var harvest = creep.harvest(source);
+
+        //     switch (harvest) {
+        //         case ERR_NOT_IN_RANGE: creep.moveTo(source); creep.say('Moving'); return;
+        //         case OK: return;
+        //         default: break;
+        //     }
+        // }
     }
 };
 
