e0006fce234f054d483b5331a4fd14c6dcd88694
James Poulakos <admin@jimpoulakos.com>
2016-11-26 12:27:08 -0600

    fix: update logic to use new message bus

--- role.upgrader.v2.js
+++ role.upgrader.v2.js
@@ -1,11 +1,14 @@
-var MessageBus = require('utility.message-bus');
+var MessageBus = require('utility.message-bus'),
+    Constants = require('constants'),
+    StringBuilder = require('utility.strings');
 
 var Upgrader = {
+    log: function (m) { StringBuilder.log('upgrader', m); },
     role: 'upgrader.v2',
     getOptimalBuild: function (energy) {
-        var workParts = ((energy - 100) / 100)|0,
-            carryParts = ((energy - 150) / 50)|0,
-            moveParts = ((energy - 150) / 50)|0;
+        var workParts = ((energy - (Constants.PartCosts.CARRY + Constants.PartCosts.MOVE)) / Constants.PartCosts.WORK)|0,
+            carryParts = ((energy - (Constants.PartCosts.WORK + Constants.PartCosts.MOVE)) / Constants.PartCosts.CARRY)|0,
+            moveParts = ((energy - (Constants.PartCosts.WORK + Constants.PartCosts.CARRY)) / Constants.PartCosts.MOVE)|0;
 
         workParts = workParts > 8? 8: workParts;
         moveParts = moveParts > 10? 10: moveParts;
@@ -21,20 +24,14 @@ var Upgrader = {
         switch (result) {
             case ERR_NOT_IN_RANGE: 
                 creep.moveTo(creep.room.controller);
-                creep.say('Moving...');
                 break;
             case ERR_NOT_ENOUGH_ENERGY:
-                if (!creep.memory.sentMessage) {
-                    MessageBus.Send({
-                        event: 'NEED_ENERGY',
-                        id: creep.name
-                    });
-                    creep.say('Need Energy');
-                    creep.memory.sentMessage = true;
+                if (!creep.memory.request) {
+                    this.log(`${creep.name} is requesting energy be delivered.`);
+                    MessageBus.Request(creep.name, RESOURCE_ENERGY);
+                    creep.memory.request = true;
                 }
-                break;
-            case OK:
-                creep.say('Upgrade');
+
                 break;
         }
     }
