b0f63ccdc0cee452404e3e9006b79c019699a14d
James Poulakos <admin@jimpoulakos.com>
2016-11-26 12:29:33 -0600

    feat: update to v2 message bus logic for request handling

--- utility.message-bus.js
+++ utility.message-bus.js
@@ -1,6 +1,43 @@
+var StringBuilder = require('utility.strings');
+
 var MessageBus = {
+    log: function (m) { StringBuilder.log('message-bus', m)},
     noop: {
         event: 'noop'
+    },
+    ReceiveNext: function (type) {
+        Memory.requests = Memory.requests || [];
+
+        if (Memory.requests.length) {
+            for (var i = 0; i < Memory.requests.length; ++i) {
+                if (Memory.requests[i].resource == type) {
+                    var request = Memory.requests.splice(i, 1);
+                    return request[0];
+                }
+            }
+        }
+
+        return null;
+    },
+    RedoRequest: function (request) {
+        Memory.requests.unshift(request);
+    },
+    Request: function (sender, request) {
+        Memory.requests = Memory.requests || [];
+
+        Memory.requests.push({
+            sender: sender,
+            resource: request
+        });
+    },
+    ReceiveRequest: function () {
+        Memory.requests = Memory.requests || [];
+
+        if (Memory.requests.length) {
+            return Memory.requests.shift();
+        }
+
+        return null;
     },
     Send: function (payload) {
         console.log(payload.id + ' requested ' + payload.event);
