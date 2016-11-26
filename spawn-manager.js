622e0baa961b6864998d47d29eb6c3930641567a
James Poulakos <admin@jimpoulakos.com>
2016-11-26 08:07:46 -0600

    feat: add creation logic

--- spawn-manager.js
+++ spawn-manager.js
@@ -1,3 +1,5 @@
+var CURRENT_VERSION = 'v2';
+
 var StringBuilder = require('utility.strings');
 
 var SpawnManager = {
@@ -11,12 +13,13 @@ var SpawnManager = {
 
         if (spawns.length) {
             var spawn = spawns[0];
-            var worker = require(`role.${creep}.v2`),
+            var worker = require(`role.${creep}.${CURRENT_VERSION}`),
                 build = worker.getOptimalBuild(room.energyAvailable)
 
             this.log(`Creating new ${creep} at spawn location ${spawn.name}.`);
             this.log('Optimal Build: ' + JSON.stringify(build));
-            
+
+            spawn.createCreep(build, undefined, {role: `${creep}.${CURRENT_VERSION}`});
         } else {
             this.log('There was an error retrieving spawn information.');
         }
