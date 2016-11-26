a0621a9f28e4a6aac4968f7d4e9089358d59f84f
James Poulakos <admin@jimpoulakos.com>
2016-11-26 12:28:12 -0600

    fix: refactor build code to not be so chatty

--- spawn-manager.js
+++ spawn-manager.js
@@ -1,25 +1,32 @@
 var CURRENT_VERSION = 'v2';
 
-var StringBuilder = require('utility.strings');
+var StringBuilder = require('utility.strings'),
+    Utility = require('utility');
 
 var SpawnManager = {
     log: function (message) {
         StringBuilder.log('spawn-manager', message);
     },
     Build: function (creep, room) {
-        this.log(`Building ${creep}`);
-
         var spawns = room.find(FIND_MY_SPAWNS);
 
         if (spawns.length) {
+            
             var spawn = spawns[0];
             var worker = require(`role.${creep}.${CURRENT_VERSION}`),
                 build = worker.getOptimalBuild(room.energyAvailable)
 
-            this.log(`Creating new ${creep} at spawn location ${spawn.name}.`);
-            this.log('Optimal Build: ' + JSON.stringify(build));
 
-            spawn.createCreep(build, undefined, {role: `${creep}.${CURRENT_VERSION}`});
+            var result = spawn.createCreep(Utility.generateBodyFromParts(build), undefined, {role: `${creep}.${CURRENT_VERSION}`});
+
+            switch (result) {
+                case OK: 
+                    this.log(`Building ${creep}`);
+                    this.log(`Creating new ${creep} at spawn location ${spawn.name}.`);
+                    this.log('Optimal Build: ' + JSON.stringify(build));
+                    room.AddWorker(creep, result);
+                    break;
+            }
         } else {
             this.log('There was an error retrieving spawn information.');
         }
