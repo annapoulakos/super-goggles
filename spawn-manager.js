6f434e90b0fe2957e20a74fa974d36a6861ea0d3
James Poulakos <admin@jimpoulakos.com>
2016-11-26 08:01:57 -0600

    feat: begin adding v2 spawning logic

--- spawn-manager.js
+++ spawn-manager.js
@@ -1,6 +1,25 @@
+var StringBuilder = require('utility.strings');
+
 var SpawnManager = {
-    Build: function (creep) {
-        
+    log: function (message) {
+        StringBuilder.log('spawn-manager', message);
+    },
+    Build: function (creep, room) {
+        this.log(`Building ${creep}`);
+
+        var spawns = room.find(FIND_MY_SPAWNS);
+
+        if (spawns.length) {
+            var spawn = spawns[0];
+            var worker = require(`role.${creep}.v2`),
+                build = worker.getOptimalBuild(room.energyAvailable)
+
+            this.log(`Creating new ${creep} at spawn location ${spawn.name}.`);
+            this.log('Optimal Build: ' + JSON.stringify(build));
+            
+        } else {
+            this.log('There was an error retrieving spawn information.');
+        }
     }
 };
 
