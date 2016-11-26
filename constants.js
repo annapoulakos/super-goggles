2957ab9151e09506c14b95609009b1db593154b6
James Poulakos <admin@jimpoulakos.com>
2016-11-26 07:58:28 -0600

    feat: add part costing for creep optimization methods

--- constants.js
+++ constants.js
@@ -2,6 +2,16 @@ var Constants = {
     ErrorMessages: {
         NOT_ENOUGH_ENERGY: 'Not enough energy to create new creep',
         SPAWN_BUSY: 'Spawn is busy right now.'
+    },
+    PartCosts: {
+        WORK: 100,
+        MOVE: 50,
+        CARRY: 50,
+        ATTACK: 80,
+        RANGED_ATTACK: 150,
+        HEAL: 250,
+        CLAIM: 600,
+        TOUGH: 10
     }
 };
 
