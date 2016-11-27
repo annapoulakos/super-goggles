7a35a58b74cc568c433d0c86a57e6cd35f0497a6
James Poulakos <admin@jimpoulakos.com>
2016-11-26 22:03:53 -0600

    feat: colorize outputs

--- utility.strings.js
+++ utility.strings.js
@@ -1,3 +1,6 @@
 module.exports = {
-    log: (sender, message) => { console.log(`[${sender}]: ${message}`); }
+    log: (sender, message) => { console.log(`[LOG ${sender}]: ${message}`); },
+    info: (sender, message) => { console.log(`[<span style="color: cyan;">INFO ${sender}</span>] ${message}`); },
+    warn: (sender, message) => { console.log(`[<span style="color: orange;">WARN ${sender}</span>] ${message}`); },
+    error: (sender, message) => { console.log(`[<span style="color: red;">ERROR ${sender}</span>] ${message}`); }
 };
